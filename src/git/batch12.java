25402d133b94c65e063fc4906d321b4012eff514
yousufzai8454 <yousufzai8454@gmail.com>
2022-02-16 20:48:47 -0500

    This commit has all the files and the basic files

---
+++ src/git/batch12.java
@@ -0,0 +1,11 @@
+package git;
+
+public class batch12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This might the last pracitce");
+	}
+
+}
